68146948641b4ac4e19a0e6b7f06e9abbeea832d
GraficaPT <graficapt.comercial@gmail.com>
2025-05-22 22:22:35 +0100

    3

---
+++ js/formSender.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+const form = document.getElementById('orcamentoForm');
+          
+form.addEventListener('submit', function (e) {
+    e.preventDefault(); // Impede envio imediato
+
+    const formData = new FormData(form);
+
+    fetch("https://formsubmit.co/graficapt.comercial@gmail.com", {
+    method: "POST",
+    body: formData
+    })
+    .then(response => {
+    if (response.ok) {
+        alert("Pedido de orçamento enviado com sucesso!");
+        window.location.href = "https://graficapt.vercel.app";
+    } else {
+        alert("Erro ao enviar. Tente novamente.");
+    }
+    })
+    .catch(error => {
+    alert("Erro ao enviar: " + error.message);
+    });
+})});
