e2fc916e6603601708a90d0639c6868b815e37d9
GraficaPT <graficapt.comercial@gmail.com>
2025-05-29 21:58:57 +0100

    3

--- js/footerheader.js
+++ js/footerheader.js
@@ -2,11 +2,11 @@ const topbarHTML = `
         <div class="bar">
             <img src="../imagens/logo_minimal.svg" onclick="location.href = 'index.html'">
             <div class="tabs desktop-only">
-                <a href="index.html#filter=rigidos">Suportes Rigídos</a>
-                <a href="index.html#filter=bandeiras">Bandeiras Publicitárias</a>
-                <a href="index.html#filter=sacos">Sacos</a>
-                <a href="index.html#filter=vestuario">Vestuário</a>
-                <a href="index.html#filter=all">Ver Tudo</a>
+                <a href="#filter=promo">Suportes Rigídos</a>
+                <a href="#filter=promo">Bandeiras Publicitárias</a>
+                <a href="#filter=promo">Sacos</a>
+                <a href="#filter=promo">Vestuário</a>
+                <a href="#filter=promo">Ver Tudo</a>
             </div>
             <div class="hamburger mobile-only" onclick="toggleSidebar()">☰</div>
         </div>
