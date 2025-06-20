9b182fba7d2dd8c548c37832856486528d14e030
GraficaPT <graficapt.comercial@gmail.com>
2025-06-20 17:42:08 +0100

    3

--- js/products.js
+++ js/products.js
@@ -730,7 +730,14 @@ const criarCarrossel = (imagens) => {
                 </div>
               </div>
             </div>
+            <div class="form-group">
+              <div class="overcell">
+                <input type="file" id="ficheiro" required>
+                <input type="hidden" name="link_ficheiro" id="link_ficheiro">
+                <p id="uploadStatus"></p>
 
+              </div>
+            </div>
             <div class="form-row">
               <div class="form-group">
                 <div class="overcell">
